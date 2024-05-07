84708056e22edbd80de91ef8481acc6a3748ddb4
Trent Wirth <trentondwirth@gmail.com>
2024-05-06 22:17:57 -0400

    basic build

---
+++ src/main.js
@@ -0,0 +1,27 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+      default: 'arcade',
+      arcade: {
+        gravity: { y: 200 }
+      }
+    },
+    scene: {
+      preload: function() {
+        this.load.setBaseURL('https://labs.phaser.io');
+        this.load.image('sky', 'assets/skies/space3.png');
+        this.load.image('logo', 'assets/sprites/phaser3-logo.png');
+      },
+      create: function() {
+        this.add.image(400, 300, 'sky');
+        var logo = this.physics.add.image(400, 100, 'logo');
+        logo.setVelocity(100, 200);
+        logo.setBounce(1, 1);
+        logo.setCollideWorldBounds(true);
+      }
+    }
+  };
+  
+  new Phaser.Game(config);
